30b4540a1e00bbd48024d8a01e5535fdf332c93b
maksunity <uglitskikh_maksim@mail.ru>
2020-11-10 21:38:52 +0500

    123

--- 1_1.cpp
+++ 1_1.cpp
@@ -4,17 +4,18 @@ int a,b,c;
 int main()
 {
     cin>>a>>b>>c;
-    if (a>b) & & (a>c)
+    if (a > b && a > c)
     {
-        cout<<a;
+        cout << a;
     }
-    if (a<b) & & (b>c)
-{
-    cout<<b;
-}
-if (c>b) & & (c>a)
-{
-    cout<<c;
-}
-return 0
+    if (b>a && b>c)
+    {
+        cout << b;
+    }
+    if (c > a && c > b)
+    {
+        cout << c;cls
+        
+    }
+    return 0;
 }
