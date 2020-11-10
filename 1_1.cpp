1958737811babf4ceab9f99e64c190fc27bafe2a
maksunity <uglitskikh_maksim@mail.ru>
2020-11-10 21:30:16 +0500

    z

---
+++ 1_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int a,b,c;
+int main()
+{
+    cin>>a>>b>>c;
+    if (a > b && a > c)
+    {
+        cout << a;
+    }
+    if (a < b && b > c)
+{
+    cout<<b;
+}
+if (c>b && c>a)
+{
+    cout<<c;
+}
+return 0
+}
