7bfefd78c1f0d37b375a561c52baee9494a31502
maksunity <uglitskikh_maksim@mail.ru>
2020-11-12 22:10:58 +0500

    WorkSocloseAll

--- 2_6.cpp
+++ 2_6.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#include <iostream> 
 using namespace std;
+int n;
 int main()
 {
-    
+    cin >> n;
+    for (int i=n; i>0;i--)
+    {
+        for (int j=0; j<n;j++)
+        {
+            if (j+i>=n)
+            {
+                cout << '*';
+            }
+            else
+            {
+                    cout << " ";
+            }
+            
+        }
+        cout << "\n";
+    }
+    return 0;
 }
--- 2_7_3.cpp
+++ 2_7_3.cpp
@@ -1,6 +1,35 @@
-#include <iostream>
+#include <iostream> // ГОТОА НЕ ДО КОНЦА
 using namespace std;
+int n;
 int main()
 {
-    
+    cin >> n;
+    for (int i=1; i<=n;i++)
+    {
+        cout << "* ";
+    }
+
+    for (int i=1; i<n; i++)
+    {
+        for (int j=1; j<n; j++)
+        { 
+           if (i<=j)
+           {
+               cout << " ";
+           }
+           else
+           {
+               cout << "* "; 
+               break;
+           }    
+        }
+        cout << "\n";
+    }
+
+    for (int i=1; i<=n; i++)
+    {
+        cout << "* ";
+    }
+    return 0;
 }
+
