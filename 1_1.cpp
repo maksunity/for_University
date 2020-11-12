f5378850767400296cf319835a699d8c474a3fb6
maksunity <uglitskikh_maksim@mail.ru>
2020-11-12 09:11:44 +0500

    Work

--- 1_1.cpp
+++ 1_1.cpp
@@ -3,21 +3,32 @@ using namespace std;
 int a,b,c;
 int main()
 {
-    cin>>a>>b>>c;
-    if (a > b && a > c)
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    if (a > c)
     {
-        cout << a;
-    }
-    if (b>a && b>c)
-    {
-        cout << b;
-    }
-    if (c > a && c > b)
-    {
-        cout << c;
+        if (a > b)
+        {
+            cout << a;
+        }
+        else
+        {
+            cout << b;
+        }
 
     }
+    else
+    {
+        if (b>c)
+        {
+            cout << b;
+        }
+        else 
+        {
+            cout << c;
+        }
+    }
+    
     return 0;
 }
-
-klsdjfjsdf
