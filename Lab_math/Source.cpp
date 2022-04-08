270fa21aadc7cfba64c894801baea8d6108522e6
maksunity <74254791+maksunity@users.noreply.github.com>
2022-04-08 23:41:11 +0500

    Add files via upload
    #

--- Lab_math/Source.cpp
+++ Lab_math/Source.cpp
@@ -1,34 +1,55 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
+#include <fstream>
+#include <algorithm>
 using namespace std;
-vector<vector<bool>> matrix;
-int color[10];
+int p[1000];
+int rk[1000];
 
-bool RepeatColor(int cur)
+struct edge
 {
-    for (int i = 0; i < 10; i++)
-    {
-        if (matrix[cur][i] && color[cur] == color[i])
-        {
-            return false;
-        }
+    int a, b, len;
+    bool operator<(edge& other) {
+        return len < other.len;
     }
-    return true;
+};
+
+void init_dsu() {
+    for (int i = 0; i < 1000; i++) {
+        p[i] = i;
+        rk[i] = 1;
+    }
 }
 
-void coloring()
-{
-    for (int i = 0; i < 10; i++)
-        for (int j = 1; j <= 10; j++)
-        {
-            color[i] = j;
-            if (RepeatColor(i))
-            {
-                break;
-            }
-            color[i] = 0;
+int get_way(int v) {
+    if (p[v] == v) {
+        return v;
+    }
+    else {
+        return p[v] = get_way(p[v]); 
+    }
+}
+
+bool merge(int a, int b) {
+    int a1 = get_way(a);
+    int b1 = get_way(b);
+
+    if (a1 == b1) {
+        return false;
+    }
+    else {
+        if (rk[a1] < rk[b1]) {
+            p[a1] = b1;
         }
+        else if (rk[b1] < rk[a1]) {
+            p[b1] = a1;
+        }
+        else {
+            p[a1] = b1;
+            rk[b1]++;
+        }
+        return true;
+    }
 }
 
 int main()
@@ -36,21 +57,30 @@ int main()
     fstream stream("f1.txt", ios::in);
     if (stream)
     {
-        for (int i = 0; i < 10; i++)
+        int size;
+        stream >> size;
+        vector<edge> edges;
+        for (int i = 0; i < size; i++)
         {
-            vector<bool> o;
-            for (int j = 0; j < 10; j++)
-            {
-                int k;
-                stream >> k;
-                o.push_back(k);
-            }
-            matrix.push_back(o);
+            edge o;
+            stream >> o.a;
+            stream >> o.b;
+            stream >> o.len;
+            edges.push_back(o);
         }
-        coloring();
-        for (int i = 0; i < 10; i++)
-        {
-            cout << i << " - " << color[i] << endl;
+        sort(edges.begin(), edges.end());
+        int mst_weight = 0;
+
+        init_dsu();
+        vector<edge> o;
+        for (edge i : edges) {
+            if (merge(i.a, i.b)) { 
+                o.push_back(i);
+            }
+        }
+        cout << "Tree: " << endl;
+        for (edge i : o) {
+            cout << i.a << " - " << i.b << " : " << i.len << endl;
         }
     }
 }
